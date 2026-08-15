590b58a5c71c35464237194601e559dd4e12e528
lordfroze <yogaadipratama22@gmail.com>
2026-08-15 17:43:09 +0700

    Fitur Payrolls - Create

--- app/Http/Controllers/PayrollController.php
+++ app/Http/Controllers/PayrollController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Payroll;
+use App\Models\Employee;
 
 class PayrollController extends Controller
 {
@@ -12,5 +13,32 @@ class PayrollController extends Controller
     {
         $payrolls = Payroll::all();
         return view('payrolls.index', compact('payrolls'));
+    }
+
+    // create
+    public function create()
+    {
+        $employees = Employee::all();
+        return view('payrolls.create', compact('employees'));
+    }
+
+    // store
+    public function store(Request $request)
+    {
+        $request->validate([
+            'employee_id' => 'required',
+            'salary' => 'required|numeric',
+            'bonuses' => 'required|numeric',
+            'deductions' => 'required|numeric',
+            'net_salary' => 'numeric|min:0',
+            'pay_date' => 'required|date',
+        ]);
+
+        // menghitung net_salary otomatis dari salary, deductions dan bonuses
+        $net_Salary = $request->input('salary') - $request->input('deductions') + $request->input('bonuses');
+        $request->merge(['net_salary' => $net_Salary]);
+
+        Payroll::create($request->all());
+        return redirect()->route('payrolls.index')->with('success', 'Payroll created successfully');
     }
 }
---
+++ resources/views/payrolls/create.blade.php
@@ -0,0 +1,116 @@
+@extends('layouts.dashboard')
+
+@section('content')
+<header class="mb-3">
+    <a href="#" class="burger-btn d-block d-xl-none">
+        <i class="bi bi-justify fs-3"></i>
+    </a>
+</header>
+
+<div class="page-heading">
+    <div class="page-title">
+        <div class="row">
+            <div class="col-12 col-md-6 order-md-1 order-last">
+                <h3>DataTable</h3>
+                <p class="text-subtitle text-muted">A sortable, searchable, paginated table without dependencies thanks to simple-datatables.</p>
+            </div>
+            <div class="col-12 col-md-6 order-md-2 order-first">
+                <nav aria-label="breadcrumb" class="breadcrumb-header float-start float-lg-end">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item"><a href="index.html">Dashboard</a></li>
+                        <li class="breadcrumb-item active" aria-current="page">DataTable</li>
+                    </ol>
+                </nav>
+            </div>
+        </div>
+    </div>
+    <section class="section">
+        <div class="card">
+            <div class="card-header">
+                <h5 class="card-title">
+                    Create Payroll
+                </h5>
+            </div>
+            <div class="card-body">
+                <!-- if error -->
+                @if($errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach($errors->all() as $error)
+                        <li>{{$error}}</li>
+                        @endforeach
+                    </ul>
+                </div>
+                @endif
+
+                <!-- form create payroll -->
+                <form action="{{route('payrolls.store')}}" method="post">
+                    @csrf
+
+                    <div class="mb-3">
+                        <label for="" class="form-label">Employee</label>
+                        <select name="employee_id" id="employee_id" class="form-control" required>
+                            @foreach($employees as $employee)
+                            <option value="{{$employee->id}}">{{$employee->fullname}}</option>
+                            @endforeach
+                        </select>
+                        @error('employee_id')
+                        <div class="invalid-feedback">{{message}}</div>
+                        @enderror
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="" class="form-label">Salary</label>
+                        <input type="number" class="form-control" name="salary" required>
+                        @error('salary')
+                        <div class="invalid-feedback">{{message}}</div>
+                        @enderror
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="" class="form-label">Deductions</label>
+                        <input type="number" class="form-control" name="deductions" required>
+                        @error('deductions')
+                        <div class="invalid-feedback">{{message}}</div>
+                        @enderror
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="" class="form-label">Bonuses</label>
+                        <input type="number" class="form-control" name="bonuses" required>
+                        @error('bonuses')
+                        <div class="invalid-feedback">{{message}}</div>
+                        @enderror
+                    </div>
+
+                    <!-- disable karena net_salary otomatis dari salary, deductions dan bonuses dari controller -->
+                    <div class="mb-3">
+                        <label for="" class="form-label">Net Salary</label>
+                        <input type="number" class="form-control" name="net_salary" disabled required>
+                        @error('net_salary')
+                        <div class="invalid-feedback">{{message}}</div>
+                        @enderror
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="" class="form-label">Pay Date</label>
+                        <input type="text" class="form-control date" name="pay_date" required>
+                        @error('pay_date')
+                        <div class="invalid-feedback">{{message}}</div>
+                        @enderror
+                    </div>
+
+
+
+
+                    <!-- tombol submit form -->
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                    <a href="{{route('payrolls.index')}}" class="btn btn-secondary">Cancel</a>
+                </form>
+
+            </div>
+        </div>
+
+    </section>
+</div>
+@endsection
