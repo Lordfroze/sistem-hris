3d11caef5a2d7c429b54046617a387cbe1a6736d
lordfroze <yogaadipratama22@gmail.com>
2026-08-16 07:15:24 +0700

    payrol update

--- app/Http/Controllers/PayrollController.php
+++ app/Http/Controllers/PayrollController.php
@@ -41,4 +41,31 @@ class PayrollController extends Controller
         Payroll::create($request->all());
         return redirect()->route('payrolls.index')->with('success', 'Payroll created successfully');
     }
+
+    // edit
+    public function edit(Payroll $payroll)
+    {
+        $employees = Employee::all();
+        return view('payrolls.edit', compact('payroll', 'employees'));
+    }
+
+    // update
+    public function update(Request $request, Payroll $payroll)
+    {
+        $request->validate([
+            'employee_id' => 'required',
+            'salary' => 'required|numeric',
+            'bonuses' => 'required|numeric',
+            'deductions' => 'required|numeric',
+            'net_salary' => 'numeric|min:0',
+            'pay_date' => 'required|date',
+        ]);
+
+        // menghitung net_salary otomatis dari salary, deductions dan bonuses
+        $net_Salary = $request->input('salary') - $request->input('deductions') + $request->input('bonuses');
+        $request->merge(['net_salary' => $net_Salary]);
+
+        $payroll->update($request->all());
+        return redirect()->route('payrolls.index')->with('success', 'Payroll updated successfully');
+    }
 }
