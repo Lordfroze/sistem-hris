89e4adfe8ff07b047dc46a35d1d1c7c044b98c72
lordfroze <yogaadipratama22@gmail.com>
2026-08-24 20:02:51 +0700

    finish

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use App\Http\Controllers\LeaveRequestController;
 
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
 });
 
 // middleware untuk check role user
